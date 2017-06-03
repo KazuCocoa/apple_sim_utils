977b4fdd7e2d5cb81dc132fdcbf1b9352d93d5b5
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-06-03 16:42:08 +0900

    arrange commands

--- lib/apple_sim_utils/command.rb
+++ lib/apple_sim_utils/command.rb
@@ -6,12 +6,14 @@ module AppleSimUtils
       @path = get_command_path
     end
 
-    private
-
     def run(*command)
-      cmd = command.join(' ')
+      cmd = ([@path] + command).join(' ')
       sto, ste, status = Open3.capture3(cmd)
       if status.success?
+        unless ste.empty?
+          puts ste
+          return ste
+        end
         sto
       else
         puts ste
@@ -19,24 +21,12 @@ module AppleSimUtils
       end
     end
 
+    private
+
     def get_command_path
       cmd = `which applesimutils`.strip
       return cmd unless cmd.empty?
       raise "You should install applesimutils. Read https://github.com/wix/AppleSimulatorUtils"
-    end
-
-    protected
-
-    def method_missing(method, *args, &_block)
-      if respond_to_missing?
-        run(%W(#{@path} #{method}) + args)
-      else
-        super
-      end
-    end
-
-    def respond_to_missing?
-      true
     end
   end
 end
